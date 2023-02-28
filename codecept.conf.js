d5bb842e61125e3efb32c91b974b6decf78ee7d6
marianadpereira <mariana.pereira@hmcts.net>
2023-02-28 11:42:29 +0000

    adding codecept files

---
+++ codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './src/test/end-to-end/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'hmc-judicial-payment-frontend',
+  mocha: {
+    reporterOptions: {
+      mochawesome: {
+        stdout: "./output/console.log",
+        options: {
+          reportDir: "./output",
+          reportFilename: "Functional Tests Report"
+        }
+      }
+    }
+  }
+}
---
+++ src/test/end-to-end/hello_world test_test.js
@@ -0,0 +1,6 @@
+Feature('hello world test @functional');
+
+Scenario('test something',  ({ I }) => {
+  I.say('Hello World');
+  I.see('hi')
+});
