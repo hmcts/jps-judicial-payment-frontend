5801b690510bddebd422aa7ac11f9ac97bd3e19c
Raakhee Rele <raakhee.rele@hmcts.net>
2023-03-06 10:22:51 +0000

    IJPS7- Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplicationRoutingComponent } from './components/routing/application-routing.component';
+import { AuthGuard } from './services/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', canActivate: [AuthGuard], component: ApplicationRoutingComponent, pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+
+  constructor( private authService: AuthService ) { }
+
+  canActivate(): Observable<boolean> {
+    return this.authService.isAuthenticated().pipe(map(isAuth => {
+      if (!isAuth) {
+        this.authService.loginRedirect();
+        return false;
+      }
+
+      return true;
+    }));
+  }
+
+ 
+}
