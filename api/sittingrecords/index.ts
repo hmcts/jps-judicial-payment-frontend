a5e4e1f99f00650637f4c96b5444acad1244da53
Raakhee Rele <raakhee.rele@hmcts.net>
2023-06-15 12:15:31 +0100

    IJPS-43 Added endpoint url for sittingrecords

--- api/sittingrecords/index.ts
+++ api/sittingrecords/index.ts
@@ -1,9 +1,8 @@
 import { getConfigValue } from '../configuration';
-import { SERVICES_LOCATION_API_PATH } from '../configuration/references';
+import { SERVICES_JPS_API_URL } from '../configuration/references';
 import axios, { AxiosRequestConfig } from 'axios';
 
-//const url: string = getConfigValue(SERVICES_LOCATION_API_PATH);
-const url: string = '';
+const url: string = getConfigValue(SERVICES_JPS_API_URL);
 
 export async function addSittingRecords(req, res) {
     const { authorization, serviceauthorization } = req.headers;
