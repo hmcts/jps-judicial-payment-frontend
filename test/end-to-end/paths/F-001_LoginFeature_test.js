16294f14cac665a4ce04dfa4eafc3cf847a07b6d
marianadpereira <mariana.pereira@hmcts.net>
2023-06-20 17:55:43 +0100

    testing failure

--- test/end-to-end/paths/F-001_LoginFeature_test.js
+++ test/end-to-end/paths/F-001_LoginFeature_test.js
@@ -10,7 +10,7 @@ Scenario('Show an error message for invalid password @F-001.2',({ I}) => {
   I.fillField('Email address', 'email-address@gmail.com');
   I.fillField('Password', '1234');
   I.click('Sign in');
-  I.see('Incorrect email or password')
+  I.see('Incorrect email or password--')
 });
 
 Scenario('Show an error message for invalid email for username @F-001.3',({ I}) => {
