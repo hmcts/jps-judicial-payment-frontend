20e246054ae88a09839a4c7e0b69e78a387495f6
marianadpereira <mariana.pereira@hmcts.net>
2023-07-14 15:31:36 +0100

    fixing scenarios

--- test/end-to-end/pages/ManageJudicialSittingRecordsPage.js
+++ test/end-to-end/pages/ManageJudicialSittingRecordsPage.js
@@ -1,11 +1,11 @@
 const { I } = inject();
 
 function addSittingRecordsInformation(tribunal, venue, day, month, year) {
-  I.amOnPage("/");
-  I.see('Manage judicial sitting records');
+  I.see('Manage Judicial Sitting Records');
   I.see('Find, add or delete judicial sitting records');
   I.selectOption('Select a tribunal service', tribunal);
   I.fillField('Select a venue', venue);
+  I.click('.mdc-list-item__primary-text');
   I.fillField('Day', day);
   I.fillField('Month', month);
   I.fillField('Year', year);
--- test/end-to-end/paths/F-006_JudicialSittingRecordsPageFeature_test.js
+++ test/end-to-end/paths/F-006_JudicialSittingRecordsPageFeature_test.js
@@ -1,40 +1,43 @@
-import {before} from "cheerio";
-
-const JudicialSittingRecordsPage = require('../pages/JudicialSittingRecordsPage');
 const ManageJudicialSittingRecordsPage = require('../pages/ManageJudicialSittingRecordsPage');
 
-Feature('Judicial Sitting Records Page Tests @functional @F-006');
-
-before(() => {
-  I.loginWithJPSRecorderUser();
-  ManageJudicialSittingRecordsPage.addSittingRecordsInformation('BBA3', 'Sutton', '11', '03', '2022');
-  I.click('Continue');
-});
+Feature('Manage Judicial Sitting Records Pages Tests @functional @F-006');
 
 Scenario('Successfully continue to "Judicial sitting records" page @S-006.1',({ I}) => {
-  I.see("Judicial sitting records");
-  I.see("Sitting records for [tribunal service], [venue], for [date]");
+  I.loginWithJPSRecorderUser();
+  ManageJudicialSittingRecordsPage.addSittingRecordsInformation('1', 'Bournemouth', '11', '03', '2022');
+  I.click('Continue');
+  I.see("Judicial Sitting Records");
+  I.see("Sitting records for 1, Bournemouth Combined Court, for 11/03/2022");
 });
 
-Scenario('Manage judicial sitting records" page is not displayed for invalid user @S-006.2',({ I}) => {
-  I.see("Judicial sitting records");
-  I.see("Sitting records for [tribunal service], [venue], for [date]");
+Scenario('Manage judicial sitting records" page is not displayed for a user that does not have recorder rolex @S-006.2',({ I}) => {
+  I.loginWithJPSPublisherUser();
+  I.dontSee('Manage Judicial Sitting Records');
 });
 
-Scenario('Venue field should be enabled only after tribunal service is selected @S-006.3',({ I}) => {
-  I.amOnPage("/");
-  I.see('Manage judicial sitting records');
+Scenario('Venue field should be enabled only after tribunal service is selected @S-006.3',  ({ I}) => {
+  I.loginWithJPSRecorderUser();
+  I.see('Manage Judicial Sitting Records');
   I.see('Find, add or delete judicial sitting records');
-  I.dontSeeElementEnabled('Select a venue');
+  I.seeElement('#venue-select[disabled]');
 });
 
 Scenario('Venue field should be reset if tribunal service is changed @S-006.4',({ I}) => {
-  I.amOnPage("/");
-  I.see('Manage judicial sitting records');
-  I.see('Find, add or delete judicial sitting records');
-  I.selectOption('Select a tribunal service', 'tribunal');
-  I.fillField('Select a venue', venue);
-  I.fillField('Day', day);
-  I.fillField('Month', month);
-  I.fillField('Year', year);
+  I.loginWithJPSRecorderUser();
+  ManageJudicialSittingRecordsPage.addSittingRecordsInformation('1', 'Bournemouth', '11', '03', '2022');
+  I.selectOption('Select a tribunal service', '2');
+  I.dontSeeInField('Select a venue','Bournemouth Combined Court');
 });
+
+Scenario('Manage judicial sitting records page will be displayed when Change is clicked @S-006.6',({ I}) => {
+  I.loginWithJPSRecorderUser();
+  ManageJudicialSittingRecordsPage.addSittingRecordsInformation('1', 'Bournemouth', '11', '03', '2022');
+  I.click('Continue');
+  I.click('Change');
+  I.seeInField('Select a tribunal service', '1');
+  I.seeInField('Select a venue','Bournemouth Combined Court');
+  I.seeInField('Day', '11');
+  I.seeInField('Month', '03');
+  I.seeInField('Year', '2022');
+});
+
