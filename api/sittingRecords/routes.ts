40cb3f730d936ee9fdafa3a08432394265506278
Josh <josh.glasgow@hmcts.net>
2023-08-08 12:40:01 +0100

    master merge fix

--- api/sittingRecords/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getSittingRecords } from './index';
-
-const sittingRecordsRouter = Router({mergeParams: true});
-
-sittingRecordsRouter.post('/searchSittingRecords', getSittingRecords);
-
-export default sittingRecordsRouter;
