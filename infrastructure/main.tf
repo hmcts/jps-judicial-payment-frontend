b9eae9b15ee71d9b9af4583738ac9c27b0ff0c3d
Josh <josh.glasgow@hmcts.net>
2023-10-24 10:11:57 +0100

    add redis

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -32,3 +32,25 @@ resource "azurerm_key_vault_secret" "jps_s2s_client_secret" {
   value        = data.azurerm_key_vault_secret.s2s_secret.value
   key_vault_id = data.azurerm_key_vault.jps_shared_key_vault.id
 }
+
+resource "azurerm_key_vault_secret" "redis6_connection_string" {
+  name         = "${var.component}-redis6-connection-string"
+  value        = "redis://${urlencode(module.redis6-cache.access_key)}@${module.redis6-cache.host_name}:${module.redis6-cache.redis_port}?tls=true"
+  key_vault_id = data.azurerm_key_vault.jps_shared_key_vault.id
+}
+
+module "redis" {
+  source                   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                  = var.product
+  location                 = var.location
+  env                      = var.env
+  common_tags              = var.common_tags
+  redis_version            = "6"
+  business_area            = "cft" # cft or sds
+  sku_name                 = var.sku_name
+  family                   = var.family
+  capacity                 = var.capacity
+
+  private_endpoint_enabled      = true
+  public_network_access_enabled = false
+}
