55b01a6c4128c76b5c7e5cab3cdeda60bd8be732
Josh <josh.glasgow@hmcts.net>
2023-10-31 11:16:58 +0000

    update test coverage

--- src/app/_workflows/recorder-workflow.service.spec.ts
+++ src/app/_workflows/recorder-workflow.service.spec.ts
@@ -6,6 +6,7 @@ import { ViewSittingRecordResponse } from '../_models/viewSittingRecords.model';
 import { ViewSittingRecordService } from '../_services/sitting-records-service/view-sitting-records-service';
 import { DateService } from '../_services/date-service/date-service';
 import { of } from 'rxjs';
+import { SittingRecord } from '../_models/sittingRecord.model';
 
 describe('RecorderWorkflowService', () => {
   let mockWorkflowService: RecorderWorkflowService;
@@ -24,7 +25,7 @@ describe('RecorderWorkflowService', () => {
     mockDateSvc = TestBed.inject(DateService);
     mockformData= new FormBuilder().group({
       dateSelected: ['2022-01-01'],
-      tribunalService: ['Tribunal 1'],
+      tribunalService: [{ serivce: 'Tribunal 1',hmctsServiceCode: '1234' }],
       venue: ['Venue 1'],
     });
 
@@ -47,6 +48,37 @@ describe('RecorderWorkflowService', () => {
       expect(mockWorkflowService.getFormData()).toBe(mockformData);
     });
   });
+
+  describe('getHmctsServiceCode', () => {
+    it('should return the service code on getHmctsServiceCode', () => {
+      expect(mockWorkflowService.getHmctsServiceCode()).toEqual('1234')
+    })
+  })
+
+  describe('delete functions', () => {
+    it('should set sittingRecordToDelete to undefined on resetSittingRecordToDelete', () => {
+      mockWorkflowService.setSittingRecordToDelete(mockRecord)
+      expect(mockWorkflowService.getSittingRecordToDelete()).toEqual(mockRecord)
+      mockWorkflowService.resetSittingRecordToDelete()
+      expect(mockWorkflowService.sittingRecordToDelete).toBe(undefined)
+
+    })
+  })
+
+  describe('checkCameFromConfirm', () => {
+    it('should return cameFromConfirm on checkCameFromConfirm', () => {
+      expect(mockWorkflowService.checkCameFromConfirm()).toBe(mockWorkflowService.cameFromConfirm)
+    })
+  })
+
+  describe('sittingRecordRoleList', () => {
+    it('should set sittingRecordsRoleList to undefined on resetSittingRecordsRoleList', () => {
+      mockWorkflowService.sittingRecordsRoleList = ['1', '2', '3']
+      expect(mockWorkflowService.sittingRecordsRoleList).toEqual(['1', '2', '3'])
+      mockWorkflowService.resetSittingRecordsRoleList()
+      expect(mockWorkflowService.sittingRecordsRoleList).toBe(undefined)
+    })
+  })
 
   describe('resetFormData', () => {
     it('should reset the form data', () => {
@@ -152,3 +184,33 @@ describe('RecorderWorkflowService', () => {
   });
   
 });
+
+const mockRecord: SittingRecord = {
+  am: true,
+  changedByUserId: '',
+  changedByUserName: '',
+  changedDateTime: '',
+  contractTypeId: 1,
+  createdByUserId: '',
+  createdByUserName: '',
+  createdDateTime: '',
+  epimmsId: '',
+  hmctsServiceId: '',
+  judgeRoleTypeId: '',
+  personalCode: '',
+  personalName: '',
+  pm: true,
+  regionId: '',
+  regionName: '',
+  statusId: '',
+  venueName: '',
+  accountCode: '',
+  contractTypeName: null,
+  crownServantFlag: false,
+  fee: null,
+  judgeRoleTypeName: null,
+  londonFlag: false,
+  payrollId: null,
+  sittingDate: '',
+  sittingRecordId: 0
+}
