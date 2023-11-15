15893bbfda9e989725e595b03e03a9e09fed2b23
Josh <josh.glasgow@hmcts.net>
2023-11-15 11:02:57 +0000

    fix tests

--- src/app/sitting-records/compare-records/compare-records.component.spec.ts
+++ src/app/sitting-records/compare-records/compare-records.component.spec.ts
@@ -1,57 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompareRecordsComponent } from './compare-records.component';
 import { CompareRecordsWorkflowService } from 'src/app/_workflows/compare-record-workflow.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 
 describe('CompareRecordsComponent', () => {
   let component: CompareRecordsComponent;
   let fixture: ComponentFixture<CompareRecordsComponent>;
-  const mockCompareRecordsWorkflowService = jasmine.createSpyObj(['getSittingRecordsData']);
+  let comparisonWorkflow: CompareRecordsWorkflowService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompareRecordsComponent ],
-      providers: [
-        { provide: CompareRecordsWorkflowService, useValue: mockCompareRecordsWorkflowService }
-      ]
-    })
-    .compileComponents();
+      declarations: [CompareRecordsComponent],
+      imports: [HttpClientTestingModule]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CompareRecordsComponent);
     component = fixture.componentInstance;
+    comparisonWorkflow = TestBed.inject(CompareRecordsWorkflowService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should handle file selection and set page sizing', () => {
-    const mockFile = new File([''], 'test.txt', { type: 'text/plain', lastModified: Date.now() });
-    const mockEvt = { target: { files: [mockFile] } } as unknown as Event;
-    component.onFileSelected(mockEvt);
+  it('should update pageSizing when a file is selected', () => {
+    const dataTransfer = new DataTransfer()
+    dataTransfer.items.add(new File([''], 'test-file.pdf'))
 
-    expect(component.selectedFile).toEqual(mockFile);
-    expect(component.pageSizing).toBeCloseTo(mockFile.size * 1.2);
+    const inputDebugEl  = fixture.debugElement.query(By.css('input[type=file]'));
+    inputDebugEl.nativeElement.files = dataTransfer.files;
+
+    inputDebugEl.nativeElement.dispatchEvent(new InputEvent('change'));
+
+    fixture.detectChanges();
+
+    expect(component.selectedFile).toBeTruthy()
   });
 
-  it('should not upload when no file is selected', () => {
-    spyOn(console, 'log');
+  it('should not update pageSizing when no file is selected', () => {
+    const event = { target: { files: [] } } as unknown as Event;
+
+    component.onFileSelected(event);
+
+    expect(component.pageSizing).toBe(0);
+  });
+
+  it('should call onUpload when a file is selected', () => {
+    const file = new File(['test'], 'test.txt');
+    const event = { target: { files: [file] } } as unknown as Event;
+    spyOn(component, 'onUpload');
+
+    component.onFileSelected(event);
+
+    expect(component.onUpload).toHaveBeenCalled();
+  });
+
+  it('should not call onUpload when no file is selected', () => {
+    const event = { target: { files: [] } } as unknown as Event;
+    spyOn(component, 'onUpload');
+
+    component.onFileSelected(event);
+
+    expect(component.onUpload).not.toHaveBeenCalled();
+  });
+
+  it('should call comparisonWorkFlow.getSittingRecordsData when onUpload is called with a selected file', () => {
+    const file = new File(['test'], 'test.txt');
+    component.selectedFile = file;
+    spyOn(comparisonWorkflow, 'getSittingRecordsData').and.returnValue(of({ sittingRecords:[] }));
 
     component.onUpload();
 
-    expect(console.log).toHaveBeenCalledWith(null);
+    expect(comparisonWorkflow.getSittingRecordsData).toHaveBeenCalledWith(0);
   });
 
-  it('should call getSittingRecordsData on upload', () => {
-    const mockFile = new File([''], 'test.txt', { type: 'text/plain', lastModified: Date.now() });
-    component.selectedFile = mockFile;
-    mockCompareRecordsWorkflowService.getSittingRecordsData.and.returnValue(of({}));
+  it('should not call comparisonWorkFlow.getSittingRecordsData when onUpload is called without a selected file', () => {
+    component.selectedFile = null;
+    spyOn(comparisonWorkflow, 'getSittingRecordsData');
 
     component.onUpload();
 
-    expect(mockCompareRecordsWorkflowService.getSittingRecordsData).toHaveBeenCalledWith(0);
+    expect(comparisonWorkflow.getSittingRecordsData).not.toHaveBeenCalled();
   });
 
-  // Additional test cases...
+
 });
